d58a12286c0e92954605f5cdeafa3c336ac00fca
Joon Park <apark702@gmail.com>
2016-09-12 15:57:20 -0400

    Create Find the outlier.js

---
+++ Find the outlier.js
@@ -0,0 +1,28 @@
+//You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+//The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+//Write a method that takes the array as an argument and returns N.
+
+function findOutlier(integers){
+  //your code here
+  var odd = [];
+  var even = [];
+  
+  function remainder(num){
+    return num%2;
+  }
+  
+  var numbers = integers.map(remainder)
+  
+  for(var i = 0; i < numbers.length; i++){
+    if (numbers[i] === 0){
+      even.push(integers[i]);
+    } else {
+      odd.push(integers[i]);
+    }
+  };
+  if (even.length === 1){
+    return even[0];
+  } else {
+    return odd[0];
+  };
+}
