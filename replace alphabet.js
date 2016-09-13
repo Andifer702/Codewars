bd9a8abb107aa65d40a9a7450d793077b70e64e3
Joon Park <apark702@gmail.com>
2016-09-13 13:42:02 -0400

    Create replace alphabet.js

---
+++ replace alphabet.js
@@ -0,0 +1,16 @@
+//Welcome. In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+//If anything in the text isn't a letter, ignore it and don't return it. a being 1, b being 2, etc. As an example:
+
+//alphabet_position("The sunset sets at twelve o' clock.")
+//Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (As a string.)
+
+function alphabetPosition(text) {
+  //filters only alphabets in an array
+  text = text.toLowerCase().replace(/[^a-z]|\s+/gi, "").split('');
+
+  function convert(num){
+    return num.charCodeAt() - 96;
+  }
+    
+    return text = text.map(convert).join(" ");
+};
